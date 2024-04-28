f565d77a51d7da4d5df5e1121ba3066f59f626ba
Venom24 <93699356+venom-24@users.noreply.github.com>
2024-04-27 23:43:16 -0600

    Update con_db.php

--- con_db.php
+++ con_db.php
@@ -1,6 +1,6 @@
 <?php
 
-$conex=mysqli_connect("http://sql211.infinityfree.com","¡if0_36445885","TB8ogBrwdMLlZ","if0_36445885_facebook"); 
+$conex=mysqli_connect("http://sql211.infinityfree.com","if0_36445885","TB8ogBrwdMLlZ","if0_36445885_facebook"); 
 
 
 
