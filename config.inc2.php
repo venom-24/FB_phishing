1d0d766acbe0eb670143fc23febab959eb0c895e
Venom24 <93699356+venom-24@users.noreply.github.com>
2024-10-05 10:55:30 -0600

    Update config.inc2.php

--- config.inc2.php
+++ config.inc2.php
@@ -29,7 +29,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = '127.0.0.1';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 /**
  * phpMyAdmin configuration storage settings.
